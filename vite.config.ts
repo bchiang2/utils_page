a68d9f01d3c6ef2a228c2fb5ddb88cb5ac6ae4a4
Benjamin Chiang <benchiang@outlook.com>
2025-08-23 22:02:02 -0700

    add shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
-import { defineConfig } from 'vite'
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   base: '/utils_page/',
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
